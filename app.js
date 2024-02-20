e450005f408d5b91786a17b9c72cf1f629588919
Jotave99 <joaobva2016@gmail.com>
2024-02-19 22:03:30 -0300

    Connecting to the MongoDB Atlas server

--- app.js
+++ app.js
@@ -9,4 +9,15 @@ const app = express();
 app.get('/', (req, res) =>{
     res.status(200).json({msg:'Bem-vindo a nossa API!'})
 })
+
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASS;
+
+mongoose.connect(
+    `mongodb+srv://${dbUser}:${dbPassword}@cluster0.yexraev.mongodb.net/?retryWrites=true&w=majority`,
+    ).then(() =>{
+    app.listen(3000)
+    console.log('Conectou ao banco!')
+}).catch((err) => console.log(err))
+
 app.listen(3000);
