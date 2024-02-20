8aefdcc97d9831096d86a6bd08daf0adc90c2bd7
Jotave99 <joaobva2016@gmail.com>
2024-02-20 16:09:36 -0300

    Created user model

--- app.js
+++ app.js
@@ -6,8 +6,36 @@ const jwt = require('jsonwebtoken');
 
 const app = express();
 
+app.use(express.json());
+
+const User = require('./models/User');
+
 app.get('/', (req, res) =>{
-    res.status(200).json({msg:'Bem-vindo a nossa API!'})
+    res.status(200).json({ msg: 'Bem-vindo a nossa API!'})
+})
+
+app.post('/auth/register', async(req,res) =>{
+
+    const {name, email, password, confirmPassword} = req.body;
+
+    if(!name) {
+        return res.status(422).json({ msg: 'O nome é obrigatório!'})
+    }
+    if(!email) {
+        return res.status(422).json({ msg: 'O email é obrigatório!'})
+    }
+    if(!password) {
+        return res.status(422).json({ msg: 'A senha é obrigatória!'})
+    }
+    if(password != confirmPassword){
+        return res.status(422).json({ msg: 'As senhas não conferem.'})
+    }
+
+    const userExists = await User.findOne({email:email});
+
+    if(userExists){
+        return res.status(422).json({ msg: 'Por favor, utilize outro email.'})
+    }
 })
 
 const dbUser = process.env.DB_USER;
@@ -20,4 +48,3 @@ mongoose.connect(
     console.log('Conectou ao banco!')
 }).catch((err) => console.log(err))
 
-app.listen(3000);
