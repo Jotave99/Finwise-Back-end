ef5bc16933355b8934bde6b9790eb0bdca1c9d26
Jotave99 <joaobva2016@gmail.com>
2024-02-21 15:34:40 -0300

    Create account

--- app.js
+++ app.js
@@ -36,6 +36,24 @@ app.post('/auth/register', async(req,res) =>{
     if(userExists){
         return res.status(422).json({ msg: 'Por favor, utilize outro email.'})
     }
+
+    const salt = await bcrypt.genSalt(12);
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    const user = new User({
+        name,
+        email,
+        password: passwordHash,
+    })
+
+    try{
+        await user.save();
+
+        res.status(201).json({msg: 'Usuário criado com sucesso!'});
+
+    }catch(error){
+        res.status(500).json({msg: 'Aconteceu um erro no servidor, tente novamente mais tarde.'})
+    }
 })
 
 const dbUser = process.env.DB_USER;
