166fa5078e6868521b531262012a860c2d64e952
Jotave99 <joaobva2016@gmail.com>
2024-02-21 21:31:20 -0300

    Token Validation

--- app.js
+++ app.js
@@ -11,39 +11,60 @@ app.use(express.json());
 const User = require('./models/User');
 
 app.get('/', (req, res) =>{
-    res.status(200).json({ msg: 'Bem-vindo a nossa API!'})
+    res.status(200).json({ msg: 'Bem-vindo a nossa API!'});
 })
 
-app.get('/user/:id', async(req,res) => {
+app.get('/user/:id', checkToken, async(req,res) => {
     const id = req.params.id;
     const user = await User.findById(id, '-password');
 
     if(!user) {
-        return res.status(404).json({ msg: 'Usuário não encontrado.'})
+        return res.status(404).json({ msg: 'Usuário não encontrado.'});
     }
-})
+
+    res.status(200).json({user});
+});
+
+function checkToken(req, res, next){
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if(!token){
+        return res.status(401).json({msg: 'Acesso negado!'});
+    }
+
+    try{
+        const secret = process.env.SECRET;
+
+        jwt.verify(token, secret);
+
+        next();
+    }catch(error){
+        res.status(400).json({msg:'Token inválido.'});
+    }
+}
 
 app.post('/auth/register', async(req,res) =>{
 
     const {name, email, password, confirmPassword} = req.body;
 
     if(!name) {
-        return res.status(422).json({ msg: 'O nome é obrigatório!'})
+        return res.status(422).json({ msg: 'O nome é obrigatório!'});
     }
     if(!email) {
-        return res.status(422).json({ msg: 'O email é obrigatório!'})
+        return res.status(422).json({ msg: 'O email é obrigatório!'});
     }
     if(!password) {
-        return res.status(422).json({ msg: 'A senha é obrigatória!'})
+        return res.status(422).json({ msg: 'A senha é obrigatória!'});
     }
     if(password != confirmPassword){
-        return res.status(422).json({ msg: 'As senhas não conferem.'})
+        return res.status(422).json({ msg: 'As senhas não conferem.'});
     }
 
     const userExists = await User.findOne({email:email});
 
     if(userExists){
-        return res.status(422).json({ msg: 'Por favor, utilize outro email.'})
+        return res.status(422).json({ msg: 'Por favor, utilize outro email.'});
     }
 
     const salt = await bcrypt.genSalt(12);
@@ -53,7 +74,7 @@ app.post('/auth/register', async(req,res) =>{
         name,
         email,
         password: passwordHash,
-    })
+    });
 
     try{
         await user.save();
@@ -62,17 +83,17 @@ app.post('/auth/register', async(req,res) =>{
 
     }catch(error){
         res.status(500).json({msg: 'Aconteceu um erro no servidor, tente novamente mais tarde.'})
-    }
-})
+    };
+});
 
 app.post('/auth/login', async(req, res) =>{
     const {email, password} = req.body;
 
     if(!email) {
-        return res.status(422).json({ msg: 'O email é obrigatório!'})
+        return res.status(422).json({ msg: 'O email é obrigatório!'});
     }
     if(!password) {
-        return res.status(422).json({ msg: 'A senha é obrigatória!'})
+        return res.status(422).json({ msg: 'A senha é obrigatória!'});
     }
 
     const user = await User.findOne({email:email});
@@ -84,7 +105,7 @@ app.post('/auth/login', async(req, res) =>{
     const checkPassword = await bcrypt.compare(password, user.password);
 
     if(!checkPassword){
-        return res.status(404).json({msg:'Senha inválida.'})
+        return res.status(404).json({msg:'Senha inválida.'});
     }
 
     try{
@@ -110,5 +131,4 @@ mongoose.connect(
     ).then(() =>{
     app.listen(3000)
     console.log('Conectou ao banco!')
-}).catch((err) => console.log(err))
-
+}).catch((err) => console.log(err));
