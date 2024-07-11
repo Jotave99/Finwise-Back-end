9ac0ec82a4135dbf6e41fc5b4f93ef900ab0ac9b
Jotave99 <joaobva2016@gmail.com>
2024-07-10 21:53:51 -0300

    Adding expense history

--- routes/expenseRoute.js
+++ routes/expenseRoute.js
@@ -47,7 +47,7 @@ router.post('/expense', async (req, res) => {
   }
 });
 
-router.get('/expense', async (req, res) => {
+router.get('/expense/currentMonth', async (req, res) => {
   try {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
@@ -59,11 +59,51 @@ router.get('/expense', async (req, res) => {
     const decoded = jwt.verify(token, process.env.SECRET);
     const userId = decoded.id;
 
-    const expenses = await Expense.find({ user: userId });
+    const startOfMonth = moment().startOf('month').toDate();
+    const endOfMonth = moment().endOf('month').toDate();
+
+    const expenses = await Expense.find({
+      user: userId,
+      date: {
+        $gte: startOfMonth,
+        $lte: endOfMonth
+      }
+    });
 
     res.status(200).json({ expenses });
   } catch (error) {
-    console.error('Erro ao obter despesas:', error);
+    console.error('Erro ao obter despesas do mês atual:', error);
+    res.status(500).json({ msg: 'Erro no servidor.' });
+  }
+});
+
+router.get('/expense/:year/:month', async (req, res) => {
+  try {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (!token) {
+      return res.status(401).json({ msg: 'Token não fornecido.' });
+    }
+
+    const decoded = jwt.verify(token, process.env.SECRET);
+    const userId = decoded.id;
+
+    const { year, month } = req.params;
+    const startOfMonth = moment(`${year}-${month}-01`, 'YYYY-MM-DD').startOf('month').toDate();
+    const endOfMonth = moment(`${year}-${month}-01`, 'YYYY-MM-DD').endOf('month').toDate();
+
+    const expenses = await Expense.find({
+      user: userId,
+      date: {
+        $gte: startOfMonth,
+        $lte: endOfMonth
+      }
+    });
+
+    res.status(200).json({ expenses });
+  } catch (error) {
+    console.error('Erro ao obter despesas de um mês específico:', error);
     res.status(500).json({ msg: 'Erro no servidor.' });
   }
 });
